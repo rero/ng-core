e706d6c3bb628483db5ac0b2e5d936575b0d1fe0
Bertrand Zuchuat <bertrand.zuchuat@rero.ch>
2019-11-13 14:23:39 +0100

    pipe: create a new getRecord pipe
    * Adds a new pipe to return a Record object given its pid and recordType.
    * This pipe is able to return a specific field.
    
    Co-Authored-by: Bertrand Zuchuat <bertrand.zuchuat@rero.ch>

---
+++ projects/rero/ng-core/src/lib/pipe/get-record.pipe.ts
@@ -0,0 +1,52 @@
+/*
+ * Invenio angular core
+ * Copyright (C) 2019 RERO
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+import { Pipe, PipeTransform } from '@angular/core';
+import { RecordService } from '../record/record.service';
+import { map } from 'rxjs/operators';
+import { extractIdOnRef } from '../utils/utils';
+
+@Pipe({
+  name: 'getRecord'
+})
+export class GetRecordPipe implements PipeTransform {
+
+  constructor(private recordService: RecordService) {}
+
+  transform(pid: any, type: string, returnType = 'object', field?: string): any {
+    // process $ref entrypoint
+    if (pid.startsWith('http')) {
+      pid = extractIdOnRef(pid);
+    }
+
+    if ('object' !== returnType) {
+      returnType = 'field';
+    }
+
+    return this.recordService.getRecord(type, pid, 1).pipe(map(data => {
+      if (!data) {
+        return null;
+      }
+      if ('object' === returnType) {
+        return data;
+      }
+      if (field in data.metadata) {
+        return data.metadata[field];
+      }
+      return null;
+    }));
+  }
+}
