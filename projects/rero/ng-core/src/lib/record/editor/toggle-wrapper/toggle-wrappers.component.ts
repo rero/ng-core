4f0a0c6826d32a48516c27587a4036f46012df36
Renaud Michotte <renaud.michotte@gmail.com>
2020-08-06 09:29:39 +0200

    editor: fix ToggleSwitch & DatePicker
    At this time, the DatePicker component reset function doesn't work as
    expected. When the reset() function is called, the value is set to
    1970-01-01 (zero epoch timestamp) instead to set an empty string into
    the field. This commit fixes this wrong behavior.
    
    This commit also fixes a problem from the ToggleSwitch component. The
    toggle switch was not correctly initialized if it is related to a simple
    component (<> Array <> Object)
    
    Co-Authored-by: Renaud Michotte <renaud.michotte@gmail.com>
    Co-Authored-by: Johnny Mariéthoz <Johnny.Mariethoz@rero.ch>

--- projects/rero/ng-core/src/lib/record/editor/toggle-wrapper/toggle-wrappers.component.ts
+++ projects/rero/ng-core/src/lib/record/editor/toggle-wrapper/toggle-wrappers.component.ts
@@ -45,10 +45,13 @@ export class ToggleWrapperComponent extends FieldWrapper implements OnInit {
     if (this.to['toggle-switch']) {
       this.tsOptions = {...this.tsOptions, ...this.to['toggle-switch']};
     }
-    /* When wrapper is initialize, we should enable the toggle if the model already contains some data.
-     * But, on init, the model isn't yet populated with data ; so we can't just check the model.
-     * The least worst solution is to subscribe to `valueChanges` observable and check the model when we receive a response
-     * Note: for a 'CustomField', it's better to implement `FormlyExtension` and use the `onPopulate` method
+    /* For simple field the ode below works fine to enable the toggle switch if needed */
+    this.tsOptions.enabled = !isEmpty(removeEmptyValues(this.formControl.value));
+    /* For complex object (array or object), it's more complex:
+     *   When wrapper is initialize, we should enable the toggle if the model already contains some data.
+     *   But, on init, the model isn't yet populated with data ; so we can't just check the model.
+     *   The least worst solution is to subscribe to `valueChanges` observable and check the model when we receive a response
+     *   Note: for a 'CustomField', it's better to implement `FormlyExtension` and use the `onPopulate` method
      */
     this.formControl.valueChanges.subscribe(
       (newValue) => {
@@ -60,8 +63,8 @@ export class ToggleWrapperComponent extends FieldWrapper implements OnInit {
   toggle(event: any) {
     if (this.tsOptions.enabled === true) {
       // toggle switch will became 'false', so just reset the field.
-      // Reseting the filed will change its value and so the `valueChanges` Observer will be called. The toggle `enabled` value will
-      // be update (to false) by this method.
+      //   Resetting the filed will change its value and so the `valueChanges` Observer will be called. The toggle `enabled` value will
+      //   be update (to false) by this method.
       this.field.formControl.reset();  // reset all children fields
     } else {
       this.tsOptions.enabled = true;
