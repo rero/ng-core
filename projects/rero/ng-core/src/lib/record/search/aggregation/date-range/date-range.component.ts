3fd45302286a5e27fb625665710e970db6e7c260
Renaud Michotte <renaud.michotte@gmail.com>
2021-07-07 15:53:16 +0200

    search: add date range facet widget
    Creates a new widget to allow filtering search result on date range
    interval.
    
    Co-authored-by: Renaud Michotte <renaud.michotte@gmail.com>

---
+++ projects/rero/ng-core/src/lib/record/search/aggregation/date-range/date-range.component.ts
@@ -0,0 +1,127 @@
+/*
+ * RERO angular core
+ * Copyright (C) 2021 RERO
+ * Copyright (C) 2021 UCLouvain
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+import { marker as _ } from '@biesbjerg/ngx-translate-extract-marker';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
+import {RecordSearchService} from '../../record-search.service';
+import {Subscription} from 'rxjs';
+import {TranslateService} from '@ngx-translate/core';
+
+@Component({
+  selector: 'ng-core-aggregation-date-range',
+  templateUrl: './date-range.component.html'
+})
+export class AggregationDateRangeComponent implements OnInit, OnDestroy {
+
+  // COMPONENTS ATTRIBUTES ====================================================
+  /** Buckets list. */
+  @Input() buckets: Array<any>;
+  /** Aggregation key. */
+  @Input() key: string;
+  /** Components configuration. */
+  @Input() config: any;
+
+  /** model used for bsDatePicker field */
+  dateRangeModel: Date[];
+  /** bsDatePicker configuration. */
+  bsConfig: any = {
+    isAnimated: true,
+    containerClass: 'theme-dark-blue'
+  };
+  /** True if route have aggregation query param. */
+  hasQueryParam = false;
+
+  /** Subscription to search service. */
+  private _searchServiceSubscription: Subscription;
+
+  // CONSTRUCTOR & HOOKS ======================================================
+  constructor(
+      private _recordSearchService: RecordSearchService,
+      private _translateService: TranslateService
+  ) { }
+
+  /** OnInit hook */
+  ngOnInit(): void {
+    // Define min/max values if not exists
+    if (!this.config.hasOwnProperty('min') || this.config.min < 0) {
+      this.config.min = 0;
+    }
+    if (!this.config.hasOwnProperty('max')) {
+      this.config.max = new Date();
+      this.config.max.setFullYear(this.config.max.getFullYear() + 1).getTime();
+    }
+
+    // Build bsDateRangePicker configuration
+    this.bsConfig.maxDate = new Date(this.config.max);
+    this.bsConfig.minDate = new Date(this.config.min);
+    const today = new Date();
+    this.bsConfig.ranges = [{
+      value: [new Date(new Date().setDate(today.getDate() - 7)), today],
+      label: this._translateService.instant(_('Last 7 days'))
+    }, {
+      value: [new Date(new Date().setDate(today.getDate() - 31)), today],
+      label: this._translateService.instant(_('Last month'))
+    }, {
+      value: [new Date(new Date().setDate(today.getDate() - 366)), today],
+      label: this._translateService.instant(_('Last year'))
+    }, {
+      value: [new Date(today.getFullYear(), today.getMonth(), 1), new Date(today.getFullYear(), today.getMonth() + 1, 0)],
+      label: this._translateService.instant(_('This month'))
+    }, {
+      value: [new Date(today.getFullYear(), 0, 1), new Date(today.getFullYear() + 1, 0, 1)],
+      label: this._translateService.instant(_('This year'))
+    }];
+
+    this.dateRangeModel = [this.config.min, this.config.max];
+    this._searchServiceSubscription = this._recordSearchService.aggregationsFilters.subscribe((filters: any) => {
+      if (!filters) {
+        return;
+      }
+      let filter = filters.find((element: any) => element.key === this.key);
+      if (filter) {
+        filter = filter.values[0].split('--').map((item: string) => {
+          return new Date(+item);
+        });
+        this.hasQueryParam = true;
+        this.dateRangeModel = filter;
+      } else {
+        this.dateRangeModel = [];
+      }
+    });
+  }
+
+  /** OnDestroy hook */
+  ngOnDestroy(): void {
+    this._searchServiceSubscription.unsubscribe();
+  }
+
+  // COMPONENTS FUNCTIONS =====================================================
+  /** Update aggregation filter. */
+  updateFilter() {
+    if (this.dateRangeModel && this.dateRangeModel[0] && this.dateRangeModel[1]) {
+      this._recordSearchService.updateAggregationFilter(this.key, [
+          `${this.dateRangeModel[0].getTime()}--${this.dateRangeModel[1].getTime()}`
+      ]);
+    }
+  }
+
+  /** Clear aggregation filter. */
+  clearFilter() {
+    this._recordSearchService.updateAggregationFilter(this.key, []);
+  }
+
+}
