8efc28dc7249973046dad075ea7d60a450cbd89a
Bertrand Zuchuat <bertrand.zuchuat@rero.ch>
2025-06-24 16:26:23 +0200

    feat(select): add disabled props
    Co-Authored-by: Bertrand Zuchuat <bertrand.zuchuat@rero.ch>

--- projects/rero/ng-core/src/lib/record/editor/formly/primeng/select/select.ts
+++ projects/rero/ng-core/src/lib/record/editor/formly/primeng/select/select.ts
@@ -118,6 +118,7 @@ export class SelectComponent extends FieldType<FormlyFieldConfig<ISelectProps>> 
   /** Default properties */
   defaultOptions: Partial<FormlyFieldConfig<ISelectProps>> = {
     props: {
+      disabled: false,
       editable: false,
       filterMatchMode: 'contains',
       fluid: true,
@@ -136,6 +137,9 @@ export class SelectComponent extends FieldType<FormlyFieldConfig<ISelectProps>> 
   ngOnInit(): void {
     const optionsObs = this.props.options;
     const changeObs = this.translateService.onLangChange.pipe(switchMap(() => this.optionValues$));
+    if (this.field.props?.disabled) {
+      this.formControl.disable();
+    }
     this.optionValues$ = merge(...[optionsObs, changeObs])
     .pipe(
       tap((options) => {
